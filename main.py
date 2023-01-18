40753b3ec7f50257cb4d9d184ffa00ca09aea57f
200402 <r_he_lol@mail.ru>
2023-01-18 16:02:16 +0500

    получение ссылкок на все вакансии

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import requests
-from  bs4 import BeautifulSoup as bs
+import config
+import time
+from bs4 import BeautifulSoup as bs
 
 
 def main():
-    name = input("Введите ссылку на первую страницу: ") + "&page="
-    a = 1+1
+    link = input("Введите ссылку: ")
+    var = getAllVacancyInOnePage(link)
+    counter = 0
+    while len(var) > 0:
+        counter += 1
+        for item in var:
+            print(item.text)
+        time.sleep(3)
+        print("---------------------------")
+        var = getAllVacancyInOnePage(link + f"&page={counter}")
+
+
+def getAllVacancyInOnePage(link):
+    return bs(requests.get(link, headers=config.headers).text, "lxml").find_all("a", class_="serp-item__title")
+
 
 if __name__ == '__main__':
     main()
-
-
-
-response = requests.get('https://ufa.hh.ru/search/vacancy', params=params, headers=headers)
